da869a0b178b229005d2dc0307a12f4beccdfae0
PhucHuynhTan <phuchtgc60244@fpt.edu.vn>
2016-05-01 02:34:18 +0700

    toolbarLayout button in maps

--- MapsPlanTravelApplication/app/src/main/java/htp/mapsplantravelapplication/MapsActivity.java
+++ MapsPlanTravelApplication/app/src/main/java/htp/mapsplantravelapplication/MapsActivity.java
@@ -13,7 +13,7 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, View.OnClickListener {
 
     private GoogleMap mMap;
     // Tooolbar
@@ -32,16 +32,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+     public  void findViewById()
+     {
+         toolbarLayout= (FABToolbarLayout)findViewById(R.id.fabtoolbar);
+     }
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
@@ -50,5 +44,18 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+            @Override
+            public void onMapLongClick(LatLng latLng) {
+                mMap.clear();
+                toolbarLayout.show();
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
