ada68007e67f943fff7372d50a954b5f51cd5b8a
PhucHuynhTan <phuchtgc60244@fpt.edu.vn>
2016-05-01 03:15:54 +0700

    toolbarLayout buton view

--- MapsPlanTravelApplication/app/src/main/java/htp/mapsplantravelapplication/MapsActivity.java
+++ MapsPlanTravelApplication/app/src/main/java/htp/mapsplantravelapplication/MapsActivity.java
@@ -13,7 +13,7 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, View.OnClickListener {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback{
 
     private GoogleMap mMap;
     // Tooolbar
@@ -26,6 +26,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        findViewById();
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -48,14 +49,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
             @Override
             public void onMapLongClick(LatLng latLng) {
-                mMap.clear();
+
                 toolbarLayout.show();
             }
         });
     }
 
-    @Override
-    public void onClick(View v) {
 
-    }
 }
